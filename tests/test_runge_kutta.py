1c150dc95ad43acb13fb48403d3f77001e02f609
FritzSven <sven-fritz@gmx.de>
2018-12-20 21:04:40 +0100

    Rename testing folder to tests folder

---
+++ tests/test_runge_kutta.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+import higher_order_CWENO_boundary_treatment.runge_kutta as rk
+
+
+def test_rk_method_consistency():
+
+    dummy_initial_data = np.array([0, 0])
+    dummy_T = 1
+    dummy_tau = 0.5
+
+    def dummy_func():
+        pass
+
+    RK3 = rk.Explicit_RK3(dummy_initial_data, dummy_T, dummy_tau, dummy_func)
+    assert np.isclose(sum(RK3.b), 1)
+
+    RK5 = rk.Explicit_RK5(dummy_initial_data, dummy_T, dummy_tau, dummy_func)
+    assert np.isclose(sum(RK5.b), 1)
+
+    RK7 = rk.Explicit_RK7(dummy_initial_data, dummy_T, dummy_tau, dummy_func)
+    assert np.isclose(sum(RK7.b), 1)
