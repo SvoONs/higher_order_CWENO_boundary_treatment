065b0df76d9c9e08e282cbc8c3f7fdddeb0063de
FritzSven <sven-fritz@gmx.de>
2018-12-22 22:30:53 +0100

    Add test case to check for correct output of CWENO operators

--- tests/test_uniform_CWENO_1D.py
+++ tests/test_uniform_CWENO_1D.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pytest
 
 from higher_order_CWENO_boundary_treatment.uniform_CWENO_1D import (
     CWENO3_1D,
@@ -9,22 +10,101 @@ from higher_order_CWENO_boundary_treatment.uniform_CWENO_1D import (
 )
 
 
-def test_operator_initialization():
+@pytest.fixture
+def data_params():
+    return {
+        "CWENO3": np.array([0, 1, 0]),
+        "CWENO5": np.array([1, 0, 1, 0, 1]),
+        "CWENO7": np.array([0, 1, 0, 1, 0, 1, 0]),
+        "h": 0.1,
+        "eps": 0.01,
+        "p": 2,
+        "d0": 0.5,
+    }
 
-    dummy_avgs = np.array([0, 0])
-    dummy_h = 0.1
-    dummy_eps = dummy_h ** 2
-    dummy_p = 2
-    dummy_d0 = 0.5
 
-    cweno3 = CWENO3_1D(dummy_avgs, dummy_h, dummy_eps, dummy_p, dummy_d0)
+@pytest.fixture
+def cweno3(data_params):
+    return CWENO3_1D(
+        data_params["CWENO3"],
+        data_params["h"],
+        data_params["eps"],
+        data_params["p"],
+        data_params["d0"],
+    )
+
+
+@pytest.fixture
+def cweno5(data_params):
+    return CWENO5_1D(
+        data_params["CWENO5"],
+        data_params["h"],
+        data_params["eps"],
+        data_params["p"],
+        data_params["d0"],
+    )
+
+
+@pytest.fixture
+def cweno7(data_params):
+    return CWENO7_1D(
+        data_params["CWENO7"],
+        data_params["h"],
+        data_params["eps"],
+        data_params["p"],
+        data_params["d0"],
+    )
+
+
+def test_operator_initialization(cweno3, cweno5, cweno7):
+
     assert 2 == cweno3.G
     assert 1 == cweno3.g
 
-    cweno5 = CWENO5_1D(dummy_avgs, dummy_h, dummy_eps, dummy_p, dummy_d0)
     assert 4 == cweno5.G
     assert 2 == cweno5.g
 
-    cweno7 = CWENO7_1D(dummy_avgs, dummy_h, dummy_eps, dummy_p, dummy_d0)
     assert 6 == cweno7.G
     assert 3 == cweno7.g
+
+
+def test_operator_result_fine(cweno3, cweno5, cweno7):
+
+    p_rec_cweno3 = cweno3.compute_reconstruction_polynomial()
+    p_rec_cweno5 = cweno5.compute_reconstruction_polynomial()
+    p_rec_cweno7 = cweno7.compute_reconstruction_polynomial()
+
+    expected_p_rec_cweno3 = [1.00056332e00, 3.75247204e-18, -6.75985268e-01]
+    assert [
+        np.isclose(expected_coeff, actual_coeff)
+        for expected_coeff in expected_p_rec_cweno3
+        for actual_coeff in p_rec_cweno3
+    ]
+
+    expected_p_rec_cweno5 = [
+        1.04785750e00,
+        2.76475774e-16,
+        -5.74321099e01,
+        1.72797088e-17,
+        2.07522374e00,
+    ]
+    assert [
+        np.isclose(expected_coeff, actual_coeff)
+        for expected_coeff in expected_p_rec_cweno5
+        for actual_coeff in p_rec_cweno5
+    ]
+
+    expected_p_rec_cweno7 = [
+        1.06630815e00,
+        -5.55224086e-16,
+        -7.95702389e01,
+        -3.46451877e-15,
+        3.07275704e-01,
+        -8.72244617e-18,
+        -2.23473239e00,
+    ]
+    assert [
+        np.isclose(expected_coeff, actual_coeff)
+        for expected_coeff in expected_p_rec_cweno7
+        for actual_coeff in p_rec_cweno7
+    ]
